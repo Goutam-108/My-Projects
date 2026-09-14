c9b4d81532ca1d2bb14ddc584fedce4db345e009
Goutam <vaishnavgoutam01@gmail.com>
2026-09-14 21:20:27 +0530

    Added security features

--- spring-sec-demo/spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
@@ -1,17 +1,43 @@
 package com.example.spring_sec_demo.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailService;
+
+
+    @Bean
+    public AuthenticationProvider authProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+        return provider;
+    }
+
 
 
     //# using lambda expression
@@ -26,12 +52,58 @@ public class SecurityConfig {
 //        return http.build();
 //    }
 
-    //# using normal function
+
+//    # using lambda expression
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
-
-
+        http.csrf(customizer-> customizer.disable())
+            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+    //        http.formLogin(Customizer.withDefaults());
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
+
+    //# using normal function
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+//
+//        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+//            @Override
+//            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+//                configurer.disable();
+//            }
+//        };
+//        http.csrf(custCsrf);
+//
+//        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> custHttp = new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+//            @Override
+//            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+//                registry.anyRequest().authenticated();
+//            }
+//        };
+//        http.authorizeHttpRequests(custHttp);
+//
+//        return http.build();
+//    }
+
+
+//    @Bean                //these are static values
+//    public UserDetailsService userDetailsService(){
+//        UserDetails user = User
+//                .withDefaultPasswordEncoder()
+//                .username("goutam")
+//                .password("g@123")
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User
+//                .withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("admin@123")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user,admin);
+//    }
 }
